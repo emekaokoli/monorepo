fba87878503af9e2eb58e8a94e9e4746de083584
Emeka Okoli <emekaokoli00@gmail.com>
2025-08-04 22:21:21 +0100

    fixed imports

--- apps/server/config/default.example.ts
+++ apps/server/config/default.example.ts
@@ -1,6 +1,6 @@
 // Example config for CI and development. DO NOT USE FOR PRODUCTION.
-export default {
-  accessTokenPrivateKey: "dummy_jwt_private_key",
-  accessTokenPublicKey: "dummy_jwt_public_key",
-  accessTokenTtl: "dummy_jwt_ttl",
+export const config = {
+  accessTokenPrivateKey: 'dummy_jwt_private_key',
+  accessTokenPublicKey: 'dummy_jwt_public_key',
+  accessTokenTtl: 'dummy_jwt_ttl',
 };
