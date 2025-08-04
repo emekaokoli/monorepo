94e3568c2b7647197809690ca1383f42465224ef
Emeka Okoli <emekaokoli00@gmail.com>
2025-08-04 15:20:01 +0100

    removed husky and added simple-git-hooks

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -8,13 +8,13 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    tailwindcss(),
     // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
     tanstackRouter({
       target: 'react',
       autoCodeSplitting: true,
     }),
+    react(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
